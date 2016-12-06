af9affdc30eafe8e52e1e4b80636bf46277961bb
Kleem Loyd Baricuatro <iamkleem@gmail.com>
2016-12-06 23:19:26 +0800

    User Login

---
+++ UserLogin/src/com/example/userlogin/MainActivity.java
@@ -0,0 +1,56 @@
+package com.example.userlogin;
+
+import android.os.Bundle;
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+@SuppressLint("CutPasteId") public class MainActivity extends Activity implements OnClickListener {
+	
+	EditText txtUsername, pwdPassword;
+	Button btnLogin;
+	 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        
+        this.txtUsername=(EditText) this.findViewById(R.id.editText1);
+        this.pwdPassword=(EditText) this.findViewById(R.id.editText2);
+        
+        this.btnLogin=(Button) this.findViewById(R.id.button1);
+        
+        this.btnLogin.setOnClickListener(this);
+    }
+
+
+
+	@Override
+	public void onClick(View arg0) {
+		// TODO Auto-generated method stub
+		String username=this.txtUsername.getText().toString();
+		String password=this.pwdPassword.getText().toString();
+		
+		if((username.equals("admin"))&&(password.equals("helloworld"))){
+			
+			Toast.makeText(this, "SUCCESS!", Toast.LENGTH_SHORT).show();
+		}
+		else{
+			AlertDialog.Builder builder=new AlertDialog.Builder(this);
+				builder.setTitle("Error");
+				builder.setMessage("Invalid User!");
+				builder.setNeutralButton("Okey", null);
+				
+			AlertDialog dialog = builder.create();
+				dialog.show();
+			
+		}
+		
+	}
+    
+}
